e599a84673ef46a547e1a6f283de95283346e1d9
Narayan prasad shukla <shuklanarayan@076gmail.com>
2026-04-29 18:50:18 +0530

    nodmailer updated

--- Backend/src/services/mail.service.js
+++ Backend/src/services/mail.service.js
@@ -3,11 +3,8 @@ import nodemailer from "nodemailer"
 const transporter = nodemailer.createTransport({
     service: "gmail",
     auth:{
-        type:'OAuth2',
         user: process.env.GOOGLE_USER,
-        clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-        refreshToken:process.env.GOOGLE_REFRESH_TOKEN,
-        clientId: process.env.GOOGLE_CLIENT_ID
+        pass: process.env.GOOGLE_PASSWORD
     }
 })
 
